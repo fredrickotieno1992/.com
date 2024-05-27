9b182aa95edc2bf0972a0e6ef46088011c597683
fredrickotieno1992 <maggie.muthoni021@gmail.com>
2024-05-27 17:49:28 +0300

    Create 3_📚_Education.py

---
+++ pages/3_📚_Education.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="fredrick nyangacha", page_icon="🌍", layout="wide")
+
+st.title("Education Background")
+st.write("Bsc,Applied Statistics with IT 📔")
+
+st.title("Certification")
+st.write("1.Certificate in Data Science with Python 📊")
+st.write("2.Certificate in machine learning with Python 🏫")
+st.write("3.Certificate in result based Monitoring and Evaluation 👁️")
