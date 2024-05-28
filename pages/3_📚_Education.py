510b4a0dfbc44d49a1c2dd9c00e487c1c21ba9bb
fredrickotieno1992 <maggie.muthoni021@gmail.com>
2024-05-28 14:53:28 +0300

    Update 3_📚_Education.py

--- pages/3_📚_Education.py
+++ pages/3_📚_Education.py
@@ -3,9 +3,15 @@ import streamlit as st
 st.set_page_config(page_title="fredrick nyangacha", page_icon="🌍", layout="wide")
 
 st.title("Education Background")
+with st.container():
+    st.write("---")
 st.write("Bsc,Applied Statistics with IT 📔")
+with st.container():
+    st.write("---")
 
 st.title("Certification")
 st.write("1.Certificate in Data Science with Python 📊")
 st.write("2.Certificate in machine learning with Python 🏫")
 st.write("3.Certificate in result based Monitoring and Evaluation 👁️")
+with st.container():
+    st.write("---")
